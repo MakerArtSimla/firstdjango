344aa9aff402fe35de4869898bbecd1c2ea7844f
Benedicte Florin <bene.florin@gmail.com>
2017-12-18 15:09:04 +1300

    Implemented template inheritance throughout the site, including both apps, Blog and Portfolio. Also used Bootstrap for styling.

--- firstdjango/views.py
+++ firstdjango/views.py
@@ -1,11 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
-    # return HttpResponse("Welcome to my Home Page!")
 
 # In alphabetical order from here onwards
 def about(request):
-    return HttpResponse("It's all about me... :-)")
+    return render(request, 'about.html')
